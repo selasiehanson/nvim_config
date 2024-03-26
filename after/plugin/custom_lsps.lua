95238a73354fb805881c27608da2ee924d0679f9
Selasie Hanson <selasie.hanson@ceros.com>
2024-03-26 09:43:20 -0700

    bring back the old lsp servers

---
+++ after/plugin/custom_lsps.lua
@@ -0,0 +1,41 @@
+local lspconfig = require("lspconfig");
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+
+
+local ocaml_lsp_command = { 'ocamllsp', '--fallback-read-dot-merlin' } -- fallback-read-dot-merlin  is needed for melange
+-- local ocaml_lsp_command = { 'ocamllsp' }   -- fallback-read-dot-merlin  is needed for melange
+lspconfig.ocamllsp.setup {
+  capabilities = capabilities,
+  cmd = ocaml_lsp_command,
+  -- on_attach = on_attach,
+}
+
+-- Mason does not support odin so we have to do it manually
+local util = require 'lspconfig.util'
+
+lspconfig['ols'].setup {
+  -- on_attach = on_attach,
+  filetypes = { "odin" },
+  rootPatterns = { "ols.json" },
+  settings = {
+  }
+}
+
+
+
+-- golangci
+lspconfig.gopls.setup {
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+
+-- gleam
+lspconfig.gleam.setup {
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gleam", "lsp" },
+}

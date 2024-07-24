4ce979f0aadec1bc204215fdb30aef0cd8a87d6f
Selasie Hanson <selasie.hanson@ceros.com>
2024-07-23 17:39:07 -0700

    deno lsp and ts server lsp

--- after/plugin/custom_lsps.lua
+++ after/plugin/custom_lsps.lua
@@ -59,3 +59,17 @@ lspconfig.gleam.setup {
 }
 
 -- lspconfig.metals.setup{}
+--
+--
+lspconfig.denols.setup {
+  capabilities = capabilities,
+  -- on_attach = on_attach,
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+lspconfig.tsserver.setup {
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false
+}

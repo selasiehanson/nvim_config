dbab92c29a37f0c2dc5607a0774ee25a052e764d
Selasie Hanson <selasie.hanson@ceros.com>
2026-06-29 16:39:06 -0700

    updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,152 +3,159 @@
 --
 -- See the kickstart.nvim README for more information
 local custom_plugins = function(use)
-	local local_plugin_directory = vim.fn.getenv("MY_NVIM_PLUGINS")
-	--
-	-- print(local_plugin_directory)
-	local out = {}
-	if local_plugin_directory ~= vim.NIL then
-		local local_plugins = { "greeter.nvim", "gitbrowser.nvim", "evaluator.nvim", "flix.nvim" }
+  local local_plugin_directory = vim.fn.getenv "MY_NVIM_PLUGINS"
+  --
+  -- print(local_plugin_directory)
+  local out = {}
+  if local_plugin_directory ~= vim.NIL then
+    local local_plugins = { "greeter.nvim", "gitbrowser.nvim", "evaluator.nvim", "flix.nvim" }
 
-		for _, plugin in ipairs(local_plugins) do
-			local pl = { dir = local_plugin_directory .. "/" .. plugin }
-			table.insert(out, pl)
-		end
-	end
+    for _, plugin in ipairs(local_plugins) do
+      local pl = { dir = local_plugin_directory .. "/" .. plugin }
+      table.insert(out, pl)
+    end
+  end
 
-	return out
+  return out
 end
 local all_plugins = {
-	-- Git related plugins
-	{ 'tpope/vim-fugitive' },
-	{ 'Airbus5717/c3.vim' },
-	{ 'tpope/vim-rhubarb' },
-	{
-		'nvimdev/lspsaga.nvim',
-		dependencies = {
-			{ 'nvim-treesitter/nvim-treesitter' },
-			{ 'nvim-tree/nvim-web-devicons' },
-		}
-	},
-	{
-		"folke/noice.nvim",
+  -- Git related plugins
+  { "tpope/vim-fugitive" },
+  { "sindrets/diffview.nvim" },
+  { "tpope/vim-rhubarb" },
+  {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter" },
+      { "nvim-tree/nvim-web-devicons" },
+    },
+  },
+  {
+    "folke/noice.nvim",
 
-		dependencies = {
-			{ "MunifTanjim/nui.nvim" },
-			{ "rcarriga/nvim-notify" },
-			{ "nvim-lua/plenary.nvim" },
-		},
-		--
-	},
-	{ "Olical/conjure" },
-	{ "gleam-lang/gleam.vim" },
+    dependencies = {
+      { "MunifTanjim/nui.nvim" },
+      { "rcarriga/nvim-notify" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    --
+  },
+  { "Olical/conjure" },
+  { "gleam-lang/gleam.vim" },
 
-	{ "tikhomirov/vim-glsl" },
-	{
-		"szw/vim-maximizer",
-	},
+  { "tikhomirov/vim-glsl" },
+  {
+    "szw/vim-maximizer",
+  },
 
-	{
-		"ntpeters/vim-better-whitespace",
-	},
+  { "Airbus5717/c3.vim" },
+  {
+    "ntpeters/vim-better-whitespace",
+  },
 
-	{
-		"alaviss/nim.nvim",
-	},
+  {
+    "alaviss/nim.nvim",
+  },
 
-	{ "b4winckler/vim-objc" },
+  { "b4winckler/vim-objc" },
 
-	{ "ziglang/zig.vim" },
-	{ "Nymphium/vim-koka" },
-	{
-		"Tetralux/odin.vim",
-	},
+  { "ziglang/zig.vim" },
+  { "Nymphium/vim-koka" },
+  {
+    "Tetralux/odin.vim",
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- optional
-		},
-	},
-	{ "onyx-lang/onyx.vim" },
-	-- { 'github/copilot.vim' },
-	{
-		"scalameta/nvim-metals",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		ft = { "scala", "sbt", "java" },
-		opts = function()
-			local metals_config = require("metals").bare_config()
-			metals_config.on_attach = function(client, bufnr)
-				-- your on_attach function
-			end
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
+  },
+  { "onyx-lang/onyx.vim" },
+  -- { 'github/copilot.vim' },
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.on_attach = function(client, bufnr)
+        -- your on_attach function
+      end
 
-			return metals_config
-		end,
-		config = function(self, metals_config)
-			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = self.ft,
-				callback = function()
-					require("metals").initialize_or_attach(metals_config)
-				end,
-				group = nvim_metals_group,
-			})
-		end
-	},
-	{ 'ovikrai/mojo-syntax' },
-	{
-		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			-- require("go").setup()
-		end,
-		event = { "CmdlineEnter" },
-		ft = { "go", 'gomod' },
-		-- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-	},
-	{
-		'joerdav/templ.vim'
-	},
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+  { "ovikrai/mojo-syntax" },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      -- require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "joerdav/templ.vim",
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 
-	-- Colors
-	{ "catppuccin/nvim",     name = "catppuccin", priority = 1000 },
-	{ "Mofiqul/dracula.nvim" },
+  -- Colors
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "Mofiqul/dracula.nvim" },
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-		"shaunsingh/nord.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- Load the colorscheme here
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+    "shaunsingh/nord.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- Load the colorscheme here
 
-			-- You can configure highlights by doing something like
-			-- vim.cmd.hi("Comment gui=none")
-		end,
-	},
-	{ "kchmck/vim-coffee-script" },
+      -- You can configure highlights by doing something like
+      -- vim.cmd.hi("Comment gui=none")
+    end,
+  },
+  { "kchmck/vim-coffee-script" },
 
-
-
-
-	-- {
-	-- 	"christoomey/vim-tmux-navigator"
-	-- }
+  -- {
+  -- 	"christoomey/vim-tmux-navigator"
+  -- }
 }
 
 local all_custom_plugins = custom_plugins()
 
 for index, value in ipairs(all_custom_plugins) do
-	table.insert(all_plugins, value)
+  table.insert(all_plugins, value)
 end
 
 return all_plugins
